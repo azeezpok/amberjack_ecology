4177ffe907d5c60e4c1319c040b05bfaa0b824ad
Abdul Azeez <43782303+azeezpok@users.noreply.github.com>
2025-07-21 18:56:34 +0530

    Add files via upload

---
+++ sp_diversity.R
@@ -0,0 +1,43 @@
+####################Diversity was estimated by constructing rarefaction and extrapolation 
+##curves using the multinomial probability distribution model on the Hill numbers 
+###q = 0 (species richness), q = 1 (exponential of Shannon entropy index), and 
+##q = 2 (the inverse of the Simpson concentration) for the sample-based incident data
+
+setwd("D:/Mubarak/species diversity")
+## install iNEXT package from CRAN
+#install.packages("iNEXT")
+## install the latest version from github
+#install.packages('devtools')
+#library(devtools)
+#install_github('AnneChao/iNEXT')
+## import packages
+library(iNEXT)
+library(ggplot2)
+data<-read.csv("Cumulative prey2.csv")
+#data<-data[,c(-1,-18)]
+str(data)
+head(data)
+data1<-as.data.frame(data)
+data(ciliates)
+head(ciliates)
+str(ciliates)
+#write.csv(ciliates,"ciliates.csv")
+as.incfreq(incidence_raw)
+data(incidence_raw)
+lapply(incidence_raw_data, as.incfreq)
+iNEXT(incidence_raw_data, q=0, datatype="incidence_raw")
+out <- iNEXT(incidence_raw_data, q=c(0, 1, 2), datatype="incidence_raw", endpoint=200)
+ggiNEXT(out, type=1, facet.var="Assemblage")
+
+# Load your data into a data frame
+incidence_data <- read.table("path/to/your/data.txt", header = TRUE, sep = "\t")
+
+# Rename columns if necessary
+colnames(incidence_data) <- c("OTU", "Sample", "Abundance")
+
+# Reshape the data frame so that each row represents a unique OTU-SampleID combination
+incidence_raw <- reshape(incidence_data, idvar = "OTU", timevar = "Sample", direction = "wide")
+
+# Rename columns to match the required format
+colnames(incidence_raw) <- c("OTU", paste0("SampleID", 1:ncol(incidence_raw[,-1])),
+                             paste0("Abundance", 1:ncol(incidence_raw[,-1])))
